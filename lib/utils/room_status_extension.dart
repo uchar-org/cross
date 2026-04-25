e4d78a81731ceba362d7f7195363ff1590d407c0
3nln <javohirtech@gmail.com>
2026-04-25 17:22:06 +0500

    fix: problem with undefined eventId

--- lib/utils/room_status_extension.dart
+++ lib/utils/room_status_extension.dart
@@ -59,13 +59,15 @@ extension RoomStatusExtension on Room {
     eventId ??= timeline.events.first.eventId;
 
     final lastReceipts = <User>{};
-    // now we iterate the timeline events until we hit the first rendered event
+    var found = false;
     for (final event in timeline.events) {
       lastReceipts.addAll(event.receipts.map((r) => r.user));
       if (event.eventId == eventId) {
+        found = true;
         break;
       }
     }
+    if (!found) return [];
     lastReceipts.removeWhere(
       (user) => user.id == client.userID,
     );
